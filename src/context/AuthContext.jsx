38fee590432f52ca7e18068e61629fef5d3e4fc5
ervvaa <es32541@seeu.edu.mk>
2026-05-27 00:08:55 +0200

    Created AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,20 @@
+import {createContext, useState} from "react";
+
+const AuthContext = createContext(null)
+
+export function AuthProvider({ children }) {
+    const[user,setUser] = useState(null)
+
+    const login = () => {
+        setUser({name: 'Erva Shasivari', email: 'shasivarierva@gmail.com'})
+    }
+
+    const logout = () => {
+        setUser(null)
+    }
+    return <div>
+        <AuthContext.Provider value={{user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    </div>
+}
